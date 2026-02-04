8b3e6b51d9632f80697c116396c91d4f47b176df
zoz salah <zoz.salah2222@gmail.com>
2026-02-04 23:01:51 +0200

    Enhance age calculator with seconds calculation
    Added calculation for age in seconds and refactored code structure.

--- project num 1.py
+++ project num 1.py
@@ -1,14 +1,24 @@
-print("----------------------------------")
-print("𝖠𝖦𝖤 𝖨𝖭 𝖣𝖠𝖸,𝖬𝖮𝖭𝖳𝖧,𝖶𝖤𝖤𝖪 𝖢𝖠𝖫𝖢𝖴𝖫𝖠𝖳𝖮𝖱")
-print("----------------------------------")
-
-age = input("\n Please enter your AGE : ")
-
 def ageCalc(age):
     dage = (int(age)*365) #Day counter
     mage = (int(age) * 12)#Month counter
     wage = (int(age) * 52)#Week counter
+    sage = (int(age) * 31536000) #Age in secounds
     print("\nYour age in DAYS : " , dage)
     print("\nYour age in WEEKS : " , wage)
     print("\nYour age in MONTHS : " , mage)
-ageCalc(age)
+    print("\nYour age in SECONDS : " , sage)
+
+def main():
+    pass  
+    print("---------------------------------------")
+    print("𝖠𝖦𝖤 𝖨𝖭 𝖣𝖠𝖸,𝖬𝖮𝖭𝖳𝖧,𝖶𝖤𝖤𝖪 , SEC 𝖢𝖠𝖫𝖢𝖴𝖫𝖠𝖳𝖮𝖱")
+    print("----------------------------------------")
+
+    age = input("\n Please enter your AGE : ")
+    ageCalc(age)
+   
+
+      
+    
+
+main()
